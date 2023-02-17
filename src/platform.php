8e12f79e6740c2c03bb94a7d355ca409421ee772
Michael Schantl <floss@schantl-lx.at>
2023-02-17 10:17:53 +0100

    Declare signature of platform_is_windows function

---
+++ src/platform.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpCommon\Util;
+
+/**
+ * Check if PHP is running on Windows
+ *
+ * @return bool `true` if PHP is running on Windows; `false` for other platforms
+ */
+function platform_is_windows(): bool {
+    // TODO: Implement platform_is_windows function
+}
