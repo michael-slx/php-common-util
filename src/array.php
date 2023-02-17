b44ca8974b090b65133ccf923a3b3d6c674e0bd0
Michael Schantl <floss@schantl-lx.at>
2023-02-17 12:10:52 +0100

    Declare signatures of get_by_path/set_by_path functions

---
+++ src/array.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpCommon\Util;
+
+/**
+ * Get a value from a multi-level array using a path
+ *
+ * This function navigates a multi-level tree of arrays using a given path and returns the referenced leaf or branch
+ * node. If the given path does not reference an existing node, the `$default` value is returned.
+ *
+ * Path components are separated by a dot character (`.`).
+ *
+ * @param array $subject Subject tree to be navigated
+ * @param string $path Path string
+ * @param mixed|null $default Default value
+ * @return mixed|null Referenced value if it exists; `$default` otherwise
+ */
+function array_get_by_path(array $subject, string $path, mixed $default = null): mixed {
+    // TODO: Implement array_get_by_path function
+}
+
+/**
+ * Set a value in a multi-level array using a path
+ *
+ * This function navigates a multi-level tree of arrays using a given path and sets the given value to the referenced
+ * node. If the given path does not reference an existing node, arrays will be created in place of the missing nodes.
+ *
+ * Path components are separated by a dot character (`.`).
+ *
+ * @param array& $subject Subject tree to be navigated
+ * @param string $path Path string
+ * @param mixed|null $value Value to set
+ * @return void
+ */
+function array_set_by_path(array &$subject, string $path, mixed $value): void {
+    // TODO: Implement array_set_by_path function
+}
