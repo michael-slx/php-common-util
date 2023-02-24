bbb503fe45ac46c8af73e73c7e4b0fab15de184a
Michael Schantl <floss@schantl-lx.at>
2023-02-24 10:03:56 +0100

    Declare signature of platform_require_windows function

--- src/platform.php
+++ src/platform.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace PhpCommon\Util;
 
+use PhpCommon\Util\Exception\UnsupportedPlatformException;
+
 /**
  * Check if PHP is running on Windows
  *
@@ -10,4 +12,14 @@ namespace PhpCommon\Util;
  */
 function platform_is_windows(): bool {
     return PHP_OS_FAMILY === 'Windows';
+}
+
+/**
+ * Require that this script is running on Windows
+ *
+ * @return void if this script is running on Windows
+ * @throws UnsupportedPlatformException if this script is not running on Windows
+ */
+function platform_require_windows(): void {
+    // TODO: Implement platform_require_windows function
 }
