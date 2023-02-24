ef9aab78fb472f77e69deead21ac54738e57bf3e
Michael Schantl <floss@schantl-lx.at>
2023-02-24 09:57:23 +0100

    Add UnsupportedPlatformException

---
+++ src/Exception/UnsupportedPlatformException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PhpCommon\Util\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception thrown when an operation is not supported on the current platform
+ */
+class UnsupportedPlatformException extends RuntimeException {
+}
